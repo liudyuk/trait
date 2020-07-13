3d99ef36afcad89097bf505751fd747d39865045
liudyuk <d.liu.2@bham.ac.uk>
2020-07-13 16:50:56 +0100

    test anotherr time

--- R_test.R
+++ R_test.R
@@ -1,3 +1,4 @@
-library(ISLR)
-data=iris
-head(data)
+
+#import the file
+trait<-read.table('/Users/liudy/TRY/trait2/woody_trait.0625.txt',header=TRUE, 
+                               stringsAsFactors=FALSE, sep="\t", dec=".",fileEncoding="latin1")
