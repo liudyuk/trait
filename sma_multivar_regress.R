1777673cda928a81365987dd574b16a88ec0e06b
Thomas Pugh <t.a.m.pugh@bham.ac.uk>
2020-07-22 12:52:50 +0100

    Updated sma_multivar_regress to take the number of bootstrap samples as an input and output the raw matrix of bootstrap samples in addition to the summary variables.

--- sma_multivar_regress.R
+++ sma_multivar_regress.R
@@ -1,5 +1,5 @@
 
-sma_regress_multivar <- function(yy) {
+sma_regress_multivar <- function(yy,nbtstrp) {
   # Function to calculate a trivariate SMA regression 
   # From Richter and Stavn (2014)
   # https://journals.ametsoc.org/jtech/article/31/7/1663/4627/Determining-Functional-Relations-in-Multivariate
@@ -11,7 +11,8 @@ sma_regress_multivar <- function(yy) {
   # Bootstrapped intervals are based on 10 000 samples.
   #
   # Input: yy - data.frame of input variables for regression
-  # Output: return_vals - array of intercept, slope and slope confidence intervals
+  # Input: nbtstrp - number of bootstrapped samples to take
+  # Output: return_vals - array of intercept, slope, slope confidence intervals and bootstrapped samples
   #
   # T. Pugh
   # 01.07.20
@@ -66,17 +67,16 @@ sma_regress_multivar <- function(yy) {
   
   #Generate bootstrap percentile intervals for slope estimates
   set.seed(1234)
-  boots=10000
-  slope_R.y1.boot <- numeric(boots) 
-  slope_R.y2.boot <- numeric(boots) 
+  slope_R.y1.boot <- numeric(nbtstrp) 
+  slope_R.y2.boot <- numeric(nbtstrp) 
   if (c>3) {
-    slope_R.y3.boot <- numeric(boots)
+    slope_R.y3.boot <- numeric(nbtstrp)
   }
   if (c>4) {
-    slope_R.y4.boot <- numeric(boots)
+    slope_R.y4.boot <- numeric(nbtstrp)
   }
 
-  for(i in 1:boots) {
+  for(i in 1:nbtstrp) {
     index = 1:n
     bootindex = sample(index, n, replace=T) 
     bootsample = yy[bootindex,] 
@@ -109,19 +109,23 @@ sma_regress_multivar <- function(yy) {
   
   if (c==3) {
     return_vals <- list("intercept_R"=intercept_R,"slope_R.y1"=slope_R.y1,"slope_R.y2"=slope_R.y2,
-                      "L95_R.y1"=L95_R.y1,"U95_R.y1"=U95_R.y1,"L95_R.y2"=L95_R.y2,"U95_R.y2"=U95_R.y2)
+                      "L95_R.y1"=L95_R.y1,"U95_R.y1"=U95_R.y1,"L95_R.y2"=L95_R.y2,"U95_R.y2"=U95_R.y2,
+                      "boot.y1"=slope_R.y1.boot,"boot.y2"=slope_R.y2.boot)
   }
   else if (c==4) {
     return_vals <- list("intercept_R"=intercept_R,"slope_R.y1"=slope_R.y1,"slope_R.y2"=slope_R.y2,
                         "slope_R.y3"=slope_R.y3,
                         "L95_R.y1"=L95_R.y1,"U95_R.y1"=U95_R.y1,"L95_R.y2"=L95_R.y2,"U95_R.y2"=U95_R.y2,
-                        "L95_R.y3"=L95_R.y3,"U95_R.y3"=U95_R.y3)
+                        "L95_R.y3"=L95_R.y3,"U95_R.y3"=U95_R.y3,
+                        "boot.y1"=slope_R.y1.boot,"boot.y2"=slope_R.y2.boot,"boot.y3"=slope_R.y3.boot)
   }
   else if (c==5) {
     return_vals <- list("intercept_R"=intercept_R,"slope_R.y1"=slope_R.y1,"slope_R.y2"=slope_R.y2,
                         "slope_R.y3"=slope_R.y3,"slope_R.y4"=slope_R.y4,
                         "L95_R.y1"=L95_R.y1,"U95_R.y1"=U95_R.y1,"L95_R.y2"=L95_R.y2,"U95_R.y2"=U95_R.y2,
-                        "L95_R.y3"=L95_R.y3,"U95_R.y3"=U95_R.y3,"L95_R.y4"=L95_R.y4,"U95_R.y4"=U95_R.y4)
+                        "L95_R.y3"=L95_R.y3,"U95_R.y3"=U95_R.y3,"L95_R.y4"=L95_R.y4,"U95_R.y4"=U95_R.y4,
+                        "boot.y1"=slope_R.y1.boot,"boot.y2"=slope_R.y2.boot,"boot.y3"=slope_R.y3.boot,
+                        "boot.y4"=slope_R.y4.boot)
   }
   else {return_vals=NA}
   
