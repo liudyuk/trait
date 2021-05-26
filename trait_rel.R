ab3cdbdd804a472ee72379e488910e8d99f5c093
Annemarie Eckes-Shephard <annemarie.eckes-shephard@nateko.lu.se>
2021-05-26 11:46:02 +0200

    streamline main script ( all optimisation functions now start with same settings)
    [TO DO] use set.seed() function and starting grid for all trait relationships that enable 28 PFTs, to enable better comparison in PCA afterwards
    
    [TO DO] double-check new optimisation scripts; the test plots after optimisation are not as *neat* as the KS LS starting combination ( but that might just be the result of the trait combination to start off with..)

--- trait_rel.R
+++ trait_rel.R
@@ -174,7 +174,7 @@ n_trait_sel=28#-1
 # Run for all deciduous (BT + BD) (=1), or BE (=2), or BT (=3), or BD (=4). This is used to set the maximum and minimum bounds in trait_opt().
 spec_group_sel=3
 
-outs_LSTLP <- trait_optim_bivar_startLSTLP(limitdataranges = T,propagate_uncer = T,trait_sel = F, n_trait_sel = 28, spec_group_sel = 4)
+outs_LSTLP <- trait_optim_bivar_startLSTLP(limitdataranges = limitdataranges ,propagate_uncer = propagate_uncer,trait_sel = trait_sel, n_trait_sel = n_trait_sel, spec_group_sel = spec_group_sel)
 # not very elegant.. but: this is to 'release' the output from function trait_optim_bivar_startLSTLP from a list of objects into single objects
 # single objects
 list2env(outs_LSTLP$predictors , envir = .GlobalEnv) 
@@ -223,21 +223,8 @@ if(trait_sel==F) {
 # 
 # Attempt to iteratively converge on the best fit values of LS, P50 and LMA, given known KS and TLP
 
-# Decide whether to limit the possible ranges of predicted traits to the observed values (T) or not (F)
-limitdataranges=T # Currently does not converge in uncertainty propagation if not set to T
 
-# Decide whether to run the uncertainty propagation (T) or not (F)
-propagate_uncer=T
-
-# Decide whether to run all trait combinations in the database for LS and Ks (F), or just a selection (T), T useful for generating output for LPJ-Guess
-trait_sel= T 
-# Number of combinations to select if trait_sel=T. Set to -1 for a systematic sample, >0 for a random sample of the size specified, we have created 28 PFTs.
-n_trait_sel = 28#-1
-
-# Run for all deciduous (BT + BD) (=1), or BE (=2), or BT (=3), or BD (=4). This is used to set the maximum and minimum bounds in trait_opt().
-spec_group_sel=3
-
-outs_KsTLP <- trait_optim_bivar_start_KsTLP(limitdataranges = T,propagate_uncer = T,trait_sel = T, n_trait_sel = -1, spec_group_sel = 4)
+outs_KsTLP <- trait_optim_bivar_start_KsTLP(limitdataranges = limitdataranges ,propagate_uncer = propagate_uncer,trait_sel = trait_sel, n_trait_sel = n_trait_sel, spec_group_sel = spec_group_sel)
 # not very elegant.. but: this is to 'release' the output from function trait_optim_bivar_startLSTLP from a list of objects into single objects
 # single objects
 list2env(outs_KsTLP$predictors , envir = .GlobalEnv) 
@@ -299,7 +286,7 @@ n_trait_sel=28#-1
 # Run for all deciduous (BT + BD) (=1), or BE (=2), or BT (=3), or BD (=4). This is used to set the maximum and minimum bounds in trait_opt().
 spec_group_sel=3
 
-outs_LSP50 <- trait_optim_bivar_start_LSP50(limitdataranges = T,propagate_uncer = T,trait_sel = T, n_trait_sel = -1, spec_group_sel = 4)
+outs_LSP50 <- trait_optim_bivar_start_LSP50(limitdataranges = limitdataranges ,propagate_uncer = propagate_uncer,trait_sel = trait_sel, n_trait_sel = n_trait_sel, spec_group_sel = spec_group_sel)
 # not very elegant.. but: this is to 'release' the output from function trait_optim_bivar_startLSTLP from a list of objects into single objects
 # single objects
 list2env(outs_LSP50$predictors , envir = .GlobalEnv) 
@@ -336,7 +323,7 @@ opt_test_plots_LSP50(trait_plot,
 # Optimisation with Ks and LS------------------------------------------------------------
 # Attempt to iteratively converge on the best fit values of TLP, P50 and LMA given known Ks and LS
 
-outs_LSKs <- trait_optim(limitdataranges = T,propagate_uncer = T,trait_sel = T, n_trait_sel = -1, spec_group_sel = 4)
+outs_LSKs <- trait_optim(limitdataranges = limitdataranges ,propagate_uncer = propagate_uncer,trait_sel = trait_sel, n_trait_sel = n_trait_sel, spec_group_sel = spec_group_sel)
 list2env(outs_LSKs$predictors , envir = .GlobalEnv)
 list2env(outs_LSKs$predicted , envir = .GlobalEnv)
 
